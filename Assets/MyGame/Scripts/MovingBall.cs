db0fd1a0365d2b023b7854da52c2ad5ffce358af
CarolinHollweger <carolinhollweger0@gmail.com>
2020-01-13 12:04:27 +0100

    added necessary Scripts and Sprites

---
+++ Assets/MyGame/Scripts/MovingBall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MovingBall : MonoBehaviour
+{
+    public Transform[] positions;
+    public Transform ball;
+
+    public void RandomBallPosition()
+    {
+        ball.position = positions[Random.Range(0, positions.Length)].position;
+    }
+}
